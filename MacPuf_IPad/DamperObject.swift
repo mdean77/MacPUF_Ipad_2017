d3bcd2dee2a11abf57b158886f4483abbaba3d1e
J. Michael Dean <mike.dean@hsc.utah.edu>
2015-04-11 16:48:55 -0600

    This is a base class that contains the dampChange function which is needed by all individual compartments as well as the overall Human object.  I created this class to avoid code redundancy.

---
+++ MacPuf_IPad/DamperObject.swift
@@ -0,0 +1,19 @@
+//
+//  DamperObject.swift
+//  MacPuf_IPad
+//
+//  Created by J Michael Dean on 4/11/15.
+//  Copyright (c) 2015 J Michael Dean. All rights reserved.
+//
+//	Metabolizer will inherit this function, as will Human.  Might be overkill to create
+//	this base object but eliminates duplicating this function in two places.
+
+import Foundation
+
+class DamperObject {
+	
+	func dampChange(newValue:Double, oldValue:Double, dampConstant:Double) -> Double {
+		return (newValue * dampConstant + oldValue)/(dampConstant + 1)
+	}
+	
+}
