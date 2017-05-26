47122fa882ce78532208a9e15e755cd3f8885fb9
mdean77 <mike.dean@hsc.utah.edu>
2017-05-26 14:58:27 -0600

    Indentation fixes.

--- MacPuf_IPad/ArterialPool.swift
+++ MacPuf_IPad/ArterialPool.swift
@@ -19,21 +19,21 @@ class ArterialPool: Metabolizer{
 	
 	override init(){
 		super.init()
-		amountOfOxygen = 195.3276										// MacPuf variable RO2MT  Factor 62
-		pO2 = 93.4906																// MacPuf variable RO2PR  Factor 72
-		oxygenContent = 19.5328											// MacPuf variable RO2CT  Factor 49
-		oxygenSaturation = 0												// gets fixed by calcContents
-		amountOfCO2 = 474.1322											// MacPuf variable RC2MT  Factor 63
-		pCO2 = 40.0676															// MacPuf variable RC2PR  Factor 74
-		carbonDioxideContent = 47.4132							// MacPuf variable RC2CT  Factor 78
-		bicarbonateContent = 23.8357								// MacPuf variable RC3CT  Factor 60
-		pH = 7.3937																	// MacPuf variable RPH    Factor 33
+		amountOfOxygen = 195.3276								// MacPuf variable RO2MT  Factor 62
+		pO2 = 93.4906											// MacPuf variable RO2PR  Factor 72
+		oxygenContent = 0.0 //19.5328									// MacPuf variable RO2CT  Factor 49
+		oxygenSaturation = 0									// gets fixed by calcContents
+		amountOfCO2 = 474.1322									// MacPuf variable RC2MT  Factor 63
+		pCO2 = 40.0676											// MacPuf variable RC2PR  Factor 74
+		carbonDioxideContent = 0.0 //47.4132							// MacPuf variable RC2CT  Factor 78
+		bicarbonateContent = 23.8357							// MacPuf variable RC3CT  Factor 60
+		pH = 7.3937												// MacPuf variable RPH    Factor 33
 		effluentOxygenContent = 19.5328							// MacPuf variable EO2CT  Factor 94
-		effluentCO2Content = 47.4132								// MacPuf variable EC2CT  Factor 101
-		nitrogenContent = 0.73											// MacPuf variable RN2CT  Factor 106
+		effluentCO2Content = 47.4132							// MacPuf variable EC2CT  Factor 101
+		nitrogenContent = 0.73									// MacPuf variable RN2CT  Factor 106
 		effluentNitrogenContent = 0.7257						// MacPuf same as EN2CT
-		amountOfN2 = 7.2573													// MacPuf variable RN2MT  Factor 108
-		lactateConcentration = 0.9940								// MacPuf variable RLACT  Factor 90
+		amountOfN2 = 7.2573										// MacPuf variable RN2MT  Factor 108
+		lactateConcentration = 0.9940							// MacPuf variable RLACT  Factor 90
 	}
 	
 
