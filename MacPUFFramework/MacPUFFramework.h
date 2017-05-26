86c2e1df582e6c1bdecc19120a9a775fe63d5e6e
mdean77 <mike.dean@hsc.utah.edu>
2017-05-26 06:58:15 -0600

    Built framework for testing in playground.

---
+++ MacPUFFramework/MacPUFFramework.h
@@ -0,0 +1,19 @@
+//
+//  MacPUFFramework.h
+//  MacPUFFramework
+//
+//  Created by J. Michael Dean on 5/26/17.
+//  Copyright © 2017 J Michael Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MacPUFFramework.
+FOUNDATION_EXPORT double MacPUFFrameworkVersionNumber;
+
+//! Project version string for MacPUFFramework.
+FOUNDATION_EXPORT const unsigned char MacPUFFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MacPUFFramework/PublicHeader.h>
+
+
