9cccb08c185108449e5674926e76fbff3ec29f69
mdean77 <mike.dean@hsc.utah.edu>
2017-05-26 14:59:44 -0600

    Playground settings.  Working with contents and understanding flow of scope.

--- MacPufPlayground.playground/Contents.swift
+++ MacPufPlayground.playground/Contents.swift
@@ -9,20 +9,7 @@ let Hct = 45.0
 let Hgb = 14.8
 
 let met = Metabolizer()
-met.amountOfCO2
-met.amountOfCO2
-met.amountOfOxygen
-met.carbonDioxideContent
-met.oxygenContent
-met.oxygenSaturation
-met.pCO2
-met.pH
-met.pO2
-met.bicarbonateContent
-
 met.calculateContents()
-
-met.calculateContents(temperature, DPG: DPG, Hct: Hct, Hgb: Hgb)
 met.amountOfCO2
 met.amountOfCO2
 met.amountOfOxygen
@@ -34,3 +21,27 @@ met.pH
 met.pO2
 met.bicarbonateContent
 
+let mike = Human()
+mike.arteries.calculateContents()
+mike.arteries.amountOfCO2
+mike.arteries.amountOfCO2
+mike.arteries.amountOfOxygen
+mike.arteries.carbonDioxideContent
+mike.arteries.oxygenContent
+mike.arteries.oxygenSaturation
+mike.arteries.pCO2
+mike.arteries.pH
+mike.arteries.pO2
+mike.arteries.bicarbonateContent
+
+mike.venousPool.calculateContents()
+mike.venousPool.amountOfCO2
+mike.venousPool.amountOfCO2
+mike.venousPool.amountOfOxygen
+mike.venousPool.carbonDioxideContent
+mike.venousPool.oxygenContent
+mike.venousPool.oxygenSaturation
+mike.venousPool.pCO2
+mike.venousPool.pH
+mike.venousPool.pO2
+mike.venousPool.bicarbonateContent
