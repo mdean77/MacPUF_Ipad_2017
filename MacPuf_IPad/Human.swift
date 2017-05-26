0a5e2bfae897bf5a64ff8b68ace4de60c0c862a6
mdean77 <mike.dean@hsc.utah.edu>
2017-05-26 14:59:03 -0600

    Added more parameters to Human object;  set up for receiving iterations.

--- MacPuf_IPad/Human.swift
+++ MacPuf_IPad/Human.swift
@@ -8,21 +8,73 @@
 
 import Foundation
 
+//  Note that Human is not a metabolizer object.  Only its component systems are metabolizers.
 class Human: DamperObject{
-	
-	let arteries: ArterialPool
+    // Simulation parameters
+    var totalSeconds = 0
+    var simlt = 1
+    var iterations = 0     // Valid value will be provided from simulation controller
+    
+    // Human parameters that are not body system specific
+    var age = 40
+    var weight = 70
+    var height = 178
+    var barometric = 760
+    var BARRF = 713.01
+    var DPG = 3.7843
+    var Hgb = 14.8
+    var Hct = 45.0
+    var temperature = 37.0
+    var male = 1
+    var metabolicRate = 100
+    var fitness = 28
+    var XLACT = 0.0006
+    var XC2PR = 45.4757
+    var DPH = 7.3996
+    var RCOAJ = 100
+    var XRESP = 8.8777
+    var veinDelay: [Double] = [0.0]
+    
+    // Body systems of human
+    let arteries: ArterialPool
 	let heart: Heart
 	let bag: Bag
 	let venousPool: VenousPool
+    //let brain:Brain
+    //let tissues:Tissue
+    //let ventilator:Ventilator
+    //let lungs:Lung
 	
 	override init(){
 		arteries = ArterialPool()
 		heart = Heart()
 		bag = Bag()
 		venousPool = VenousPool()
+        // brain = Brain()
+        // tissues = Tissue()
+        // ventilator = Ventilator()
+        // lungs = Lung()
+        
+        arteries.calculateContents(temperature, DPG: DPG, Hct: Hct, Hgb: Hgb)
+        // lungs.calculateContents(temperature, DPG: DPG, Hct: Hct, Hgb: Hgb)
+        // tissues.calculateContents(temperature, DPG: DPG, Hct: Hct, Hgb: Hgb)
+        // brain.calculateContents(temperature, DPG: DPG, Hct: Hct, Hgb: Hgb)
+        
+        for i in 1...10 {
+            veinDelay[i-1] = venousPool.oxygenContent
+            veinDelay[i+9] = venousPool.carbonDioxideContent
+            veinDelay[i+19] = venousPool.bicarbonateContent
+            veinDelay[i+29] = XC2PR
+        }
+        
+        
 		
 	}
 	
+    func setIterations(_ newIterations:Int){
+        iterations = newIterations
+    }
+    
 	func dummy(){
 			arteries.effluentCO2Content = 99
 		print("Value of resting cardiac output is \(heart.restingCardiacOutput)")
