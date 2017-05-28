298f69a661bce6e6f89381f2e54866a875fb1d16
J. Michael Dean <mike.dean@hsc.utah.edu>
2017-05-28 13:59:08 -0600

    Added a target for a console version of MacPuf.  This enables me to run the model separately from the XCode environment.  Obviously this is only for testing and it did identify a problem with a format string.

---
+++ MacpufConsoleTool/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  MacpufConsoleTool
+//
+//  Created by J Michael Dean on 5/28/17.
+//  Copyright © 2017 J Michael Dean. All rights reserved.
+//
+
+import Foundation
+
+let macpuf = Simulator()
+macpuf.iterations = 60
+macpuf.intervalFactor = 10
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
+macpuf.simulate()
