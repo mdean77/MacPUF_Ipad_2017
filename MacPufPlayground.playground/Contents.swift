261d205734d460c2021135553fb3200b6706841d
mdean77 <mike.dean@hsc.utah.edu>
2017-06-16 09:34:38 -0600

    Changed playground.

--- MacPufPlayground.playground/Contents.swift
+++ MacPufPlayground.playground/Contents.swift
@@ -4,6 +4,10 @@ import Foundation
 @testable import MacPUFFramework
 
 let macpuf = Simulator()
+macpuf.iterations = 180
+macpuf.intervalFactor = 10
+macpuf.simulate()
+macpuf.human.lungs.FiO2 = 10
 macpuf.simulate()
 
 
